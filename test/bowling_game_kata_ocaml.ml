c029acb4a829093a02e33a2bf9a18cf9b8b7f0de
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-26 14:56:17 +0100

    r Change indentation to 2 spaces

--- test/bowling_game_kata_ocaml.ml
+++ test/bowling_game_kata_ocaml.ml
@@ -4,8 +4,8 @@ open Bowling_game_kata.Bowling_game
 let always n _ = n
 
 let () = run_test_tt_main ("Bowling Game" >::: [
-    "A gutter game has a score of zero" >:: (fun _ ->
-        let rolls = List.init 20 @@ always 0 in
-        let result = score rolls in
-        assert_equal 0 result ~printer:string_of_int)
+  "A gutter game has a score of zero" >:: (fun _ ->
+    let rolls = List.init 20 @@ always 0 in
+    let result = score rolls in
+    assert_equal 0 result ~printer:string_of_int)
 ])
