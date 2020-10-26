eef5050beed32b928d8f12aac133cd6479b07bd3
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-27 00:13:38 +0100

    Use Fun module, remove `always` function

--- test/bowling_game_kata_ocaml.ml
+++ test/bowling_game_kata_ocaml.ml
@@ -1,21 +1,20 @@
+open Fun
 open OUnit2
 open Bowling_game_kata.Bowling_game
 
-let always n _ = n
-
 let () = run_test_tt_main ("Bowling Game" >::: [
   "A gutter game has a score of zero" >:: (fun _ ->
-    let rolls = List.init 20 @@ always 0 in
+    let rolls = List.init 20 @@ const 0 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 0 result);
 
   "A game with all rolls knocking 1 pin has a score of twenty" >:: (fun _ ->
-    let rolls = List.init 20 @@ always 1 in
+    let rolls = List.init 20 @@ const 1 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 20 result);
 
   "A game with a 5 and all zeroes rolls knocking 1 pin has a score of 5" >:: (fun _ ->
-    let rolls = 5 :: (List.init 19 @@ always 0) in
+    let rolls = 5 :: (List.init 19 @@ const 0) in
     let result = score rolls in
     assert_equal ~printer:string_of_int 5 result);
 ])
