f5839659a534a39053cf235817b94adb47cb3681
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-28 00:03:34 +0100

    r Simplify score function

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,13 +1,13 @@
-let frame_score ?(spare_bonus = 0) n_1 n_2 =
+let frame_score ?(n_3 = 0) n_1 n_2 =
   let sum = n_1 + n_2 in
   match sum with
-  | 10 -> 10 + spare_bonus
+  | 10 -> 10 + n_3
   | n -> n
 
-let rec score ?(sum = 0) = function
+let rec score = function
+  | [] -> 0
   | n_1::n_2::n_3::rest ->
-      score ~sum:(sum+frame_score n_1 n_2 ~spare_bonus:n_3) (n_3::rest)
+      frame_score n_1 n_2 ~n_3:n_3 + score (n_3::rest)
   | n_1::n_2::rest ->
-      score ~sum:(sum+frame_score n_1 n_2) rest
-  | [] -> sum
+      frame_score n_1 n_2 + score rest
   | _ -> failwith "missing arguments"
