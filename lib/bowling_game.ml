3f4854aea3f60ff76ffa8e10859e3d554b5408b2
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-27 22:27:01 +0100

    r Compute score frame by frame

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,1 +1,11 @@
-let score = List.fold_left (+) 0
+let frame_score n_1 n_2 =
+  n_1 + n_2
+
+let rec score ?sum rolls =
+  match sum with
+  | None -> score ~sum:0 rolls
+  | Some n ->
+      match rolls with
+      | n_1::n_2::rest -> score ~sum:(n+frame_score n_1 n_2) rest
+      | [] -> n
+      | _ -> failwith "missing arguments"
