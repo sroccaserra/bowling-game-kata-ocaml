8c024d15d05082282ed3a840ea5b5973fc871a6e
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-28 09:57:57 +0100

    r Simplify frame_score function

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,4 +1,4 @@
-let frame_score ?(n_3 = 0) n_1 n_2 =
+let frame_score n_1 n_2 n_3 =
   let sum = n_1 + n_2 in
   match sum with
   | 10 -> 10 + n_3
@@ -6,8 +6,7 @@ let frame_score ?(n_3 = 0) n_1 n_2 =
 
 let rec score = function
   | [] -> 0
+  | n_1::n_2::[] -> n_1 + n_2
   | n_1::n_2::n_3::rest ->
-      frame_score n_1 n_2 ~n_3:n_3 + score (n_3::rest)
-  | n_1::n_2::rest ->
-      frame_score n_1 n_2 + score rest
+      frame_score n_1 n_2 n_3 + score (n_3::rest)
   | _ -> failwith "missing arguments"
