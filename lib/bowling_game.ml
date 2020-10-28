39fc9204254da2d44a84240d99973402d7c9e0cb
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-28 16:13:23 +0100

    deleteme

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,8 +1,26 @@
-type categorie = UnePaire
 
-type rang = As
+type categorie = UnePaire | DeuxPaires
 
-type couleur = Pique | Coeur
+type rang = As | Deux | Trois | Quatre | Cinq | Six | Sept | Huit | Neuf | Dix | Valet | Dame | Roi
 
-let meilleure_combinaison _ =
-  UnePaire
+type couleur = Pique | Coeur | Trefle | Carreau
+
+type carte = Carte of rang * couleur
+
+type main = carte list
+
+let rang_de (c: carte) : rang =
+  match c with
+  | Carte (r, _) -> r
+
+let meilleure_combinaison (m: main) : categorie =
+  let rangs = List.map rang_de m in
+  match rangs with
+  | _::_::_::Deux::_ -> DeuxPaires
+  | _::_::_::Quatre::_ -> DeuxPaires
+  | _ -> UnePaire
+
+let string_of_categorie (c: categorie) : string =
+  match c with
+  | UnePaire -> "UnePaire"
+  | DeuxPaires -> "DeuxPaires"
--- test/bowling_game_kata_ocaml.ml
+++ test/bowling_game_kata_ocaml.ml
@@ -9,6 +9,18 @@ open Bowling_game_kata.Bowling_game
  *)
 let () = run_test_tt_main ("Poker hand" >::: [
   "Reconnais une paire" >:: (fun _ ->
-    let resultat = meilleure_combinaison [ (As, Pique) ; (As, Coeur) ] in
-    assert_equal UnePaire resultat);
+    let resultat = meilleure_combinaison [ Carte (As, Pique) ;  Carte (As, Coeur) ; Carte (Deux, Pique) ; Carte (Trois, Pique) ; Carte (Quatre, Pique) ] in
+    assert_equal UnePaire resultat ~printer:string_of_categorie);
+
+  "Reconnais deux paires" >:: (fun _ ->
+    let resultat = meilleure_combinaison [ Carte (As, Pique) ;  Carte (As, Coeur) ; Carte (Deux, Pique) ; Carte (Deux, Coeur) ; Carte (Quatre, Pique) ] in
+    assert_equal DeuxPaires resultat ~printer:string_of_categorie);
+
+  "Reconnais deux autres paires" >:: (fun _ ->
+    let resultat = meilleure_combinaison [ Carte (As, Pique) ;  Carte (As, Coeur) ; Carte (Deux, Pique) ; Carte (Deux, Carreau) ; Carte (Quatre, Pique) ] in
+    assert_equal DeuxPaires resultat ~printer:string_of_categorie);
+
+  "Reconnais yet again deux autres paires" >:: (fun _ ->
+    let resultat = meilleure_combinaison [ Carte (As, Pique) ;  Carte (As, Coeur) ; Carte (Deux, Pique) ; Carte (Quatre, Carreau) ; Carte (Quatre, Pique) ] in
+    assert_equal DeuxPaires resultat ~printer:string_of_categorie);
 ])
