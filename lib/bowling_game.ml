ed1431626b70ba4780017e079277f52f2b87cec0
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-28 18:43:23 +0100

    deleteme

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,12 +1,7 @@
-
 type categorie = UnePaire | DeuxPaires
-
 type rang = As | Deux | Trois | Quatre | Cinq | Six | Sept | Huit | Neuf | Dix | Valet | Dame | Roi
-
 type couleur = Pique | Coeur | Trefle | Carreau
-
 type carte = Carte of rang * couleur
-
 type main = carte list
 
 let rang_de (c: carte) : rang =
@@ -14,10 +9,11 @@ let rang_de (c: carte) : rang =
   | Carte (r, _) -> r
 
 let meilleure_combinaison (m: main) : categorie =
-  let rangs = List.map rang_de m in
+  let rangs = List.sort compare @@ List.map rang_de m in
   match rangs with
-  | _::_::_::Deux::_ -> DeuxPaires
-  | _::_::_::Quatre::_ -> DeuxPaires
+  | a :: b :: c :: d :: _ when a == b && c == d -> DeuxPaires
+  | a :: b :: _ :: d :: [e] when a == b && d == e -> DeuxPaires
+  | _ :: b :: c :: d :: [e] when b == c && d == e -> DeuxPaires
   | _ -> UnePaire
 
 let string_of_categorie (c: categorie) : string =
