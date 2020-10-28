af93e9b1c75b77d991681edf7300bd3964d4a7d0
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-28 15:09:58 +0100

    deleteme

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,12 +1,8 @@
-let frame_score n_1 n_2 n_3 =
-  let sum = n_1 + n_2 in
-  match sum with
-  | 10 -> 10 + n_3
-  | n -> n
+type categorie = UnePaire
 
-let rec score = function
-  | [] -> 0
-  | n_1::n_2::[] -> n_1 + n_2
-  | n_1::n_2::n_3::rest ->
-      frame_score n_1 n_2 n_3 + score (n_3::rest)
-  | _ -> failwith "missing arguments"
+type rang = As
+
+type couleur = Pique | Coeur
+
+let meilleure_combinaison _ =
+  UnePaire
--- test/bowling_game_kata_ocaml.ml
+++ test/bowling_game_kata_ocaml.ml
@@ -1,35 +1,14 @@
-open Fun
 open OUnit2
 open Bowling_game_kata.Bowling_game
 
-let () = run_test_tt_main ("Bowling Game" >::: [
-  "A gutter game has a score of zero" >:: (fun _ ->
-    let rolls = List.init 20 @@ const 0 in
-    let result = score rolls in
-    assert_equal ~printer:string_of_int 0 result);
-
-  "A game with all rolls knocking 1 pin has a score of twenty" >:: (fun _ ->
-    let rolls = List.init 20 @@ const 1 in
-    let result = score rolls in
-    assert_equal ~printer:string_of_int 20 result);
-
-  "A game with a 5 and all zeroes rolls knocking 1 pin has a score of 5" >:: (fun _ ->
-    let rolls = 5 :: (List.init 19 @@ const 0) in
-    let result = score rolls in
-    assert_equal ~printer:string_of_int 5 result);
-
-  "A spare as first result with a bonus of 1" >:: (fun _ ->
-    let rolls = 3::7::1::(List.init 17 @@ const 0) in
-    let result = score rolls in
-    assert_equal ~printer:string_of_int 12 result);
-
-  "A spare as second result" >:: (fun _ ->
-    let rolls = 0::0::3::7::1::(List.init 15 @@ const 0) in
-    let result = score rolls in
-    assert_equal ~printer:string_of_int 12 result);
-
-  "A spare with a bonus of 2" >:: (fun _ ->
-    let rolls = 0::0::3::7::2::(List.init 15 @@ const 0) in
-    let result = score rolls in
-    assert_equal ~printer:string_of_int 14 result);
+(*
+ * High card (aucun du reste)
+ * Une paire
+ * Deux paires
+ * Brelan
+ *)
+let () = run_test_tt_main ("Poker hand" >::: [
+  "Reconnais une paire" >:: (fun _ ->
+    let resultat = meilleure_combinaison [ (As, Pique) ; (As, Coeur) ] in
+    assert_equal UnePaire resultat);
 ])
