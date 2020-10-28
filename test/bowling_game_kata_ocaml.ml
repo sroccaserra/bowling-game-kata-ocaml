ed16d8befff9a21aded742f93b29b527ad2bf903
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-28 22:38:27 +0100

    r Add rolls_with test function

--- test/bowling_game_kata_ocaml.ml
+++ test/bowling_game_kata_ocaml.ml
@@ -2,39 +2,42 @@ open Fun
 open OUnit2
 open Bowling_game_kata.Bowling_game
 
+let rolls_with ~nb_pins ~times =
+  List.init times @@ const nb_pins
+
 let () = run_test_tt_main ("Bowling Game" >::: [
   "A gutter game has a score of zero" >:: (fun _ ->
-    let rolls = List.init 20 @@ const 0 in
+    let rolls = rolls_with ~nb_pins:0 ~times:20 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 0 result);
 
   "A game with all rolls knocking 1 pin has a score of twenty" >:: (fun _ ->
-    let rolls = List.init 20 @@ const 1 in
+    let rolls = rolls_with ~nb_pins:1 ~times:20 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 20 result);
 
   "A game with a 5 and all zeroes rolls has a score of 5" >:: (fun _ ->
-    let rolls = 5 :: (List.init 19 @@ const 0) in
+    let rolls = 5 :: rolls_with ~nb_pins:0 ~times:19 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 5 result);
 
   "A spare as first result with a bonus of 1" >:: (fun _ ->
-    let rolls = 3::7::1::(List.init 17 @@ const 0) in
+    let rolls = [3; 7; 1] @ rolls_with ~nb_pins:0 ~times:17 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 12 result);
 
   "A spare as second result" >:: (fun _ ->
-    let rolls = 0::0::3::7::1::(List.init 15 @@ const 0) in
+    let rolls = [0; 0; 3; 7; 1] @ rolls_with ~nb_pins:0 ~times:15 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 12 result);
 
   "A spare with a bonus of 2" >:: (fun _ ->
-    let rolls = 0::0::3::7::2::(List.init 15 @@ const 0) in
+    let rolls = [0; 0; 3; 7; 2] @ rolls_with ~nb_pins:0 ~times:15 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 14 result);
 
   "A strike with a bonus of 2 scores 14" >:: (fun _ ->
-    let rolls = 10::1::1::(List.init 16 @@ const 0) in
+    let rolls = [10; 1; 1] @ rolls_with ~nb_pins:0 ~times:16 in
     let result = score rolls in
     assert_equal ~printer:string_of_int 14 result);
 ])
