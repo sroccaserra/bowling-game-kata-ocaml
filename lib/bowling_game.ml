449497eaad6ea041821fb43875dc7edc374d2d79
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 20:55:55 +0100

    r Add is_strike and is_spare functions

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,10 +1,16 @@
+let is_strike =
+  (==) 10
+
+let is_spare x y =
+  x + y = 10
+
 let rec score ?(n = 1) = function
   | [] -> 0
   | [_] -> failwith "wrong number of rolls"
   | [x; y; z] when n = 10 -> x + y + z
-  | 10 :: y :: z :: rest ->
+  | x :: y :: z :: rest when is_strike x ->
     10 + y + z + score ~n:(succ n) (y :: z :: rest)
-  | x :: y :: z :: rest when x + y = 10 ->
+  | x :: y :: z :: rest when is_spare x y  ->
     10 + z + score ~n:(succ n) (z :: rest)
   | x :: y :: rest ->
      x + y + score ~n:(succ n) rest
