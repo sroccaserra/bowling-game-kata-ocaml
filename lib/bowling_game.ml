8f77c4339bf90424f5687c76997a8d791a176ec8
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 20:20:17 +0100

    r Remove frame score function

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,12 +1,3 @@
-let frame_score x y z =
-  match x with
-  | 10 -> 10 + y + z
-  | _ ->
-    let sum = x + y in
-    match sum with
-    | 10 -> 10 + z
-    | n -> n
-
 let rec score ?(n = 1) rolls =
   match rolls with
   | [] -> 0
@@ -14,6 +5,8 @@ let rec score ?(n = 1) rolls =
   | [x; y] when n = 10 -> x + y
   | [_; _] -> 0
   | 10 :: y :: z :: rest ->
-      frame_score 10 y z + score ~n:(succ n) (y :: z :: rest)
+    10 + y + z + score ~n:(succ n) (y :: z :: rest)
+  | x :: y :: z :: rest when x + y = 10 ->
+    10 + z + score ~n:(succ n) (z :: rest)
   | x :: y :: z :: rest ->
-      frame_score x y z + score ~n:(succ n) (z :: rest)
+     x + y + score ~n:(succ n) (z :: rest)
