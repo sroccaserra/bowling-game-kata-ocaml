0e022a77a9f1c2337c0799303ced6c1de83d0a57
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 20:17:52 +0100

    r Rename frame number variable

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -7,14 +7,13 @@ let frame_score x y z =
     | 10 -> 10 + z
     | n -> n
 
-let rec score ?(frame_number = 1) rolls =
-  let next_frame_number = frame_number + 1 in
+let rec score ?(n = 1) rolls =
   match rolls with
   | [] -> 0
   | [_] -> 0
-  | [x; y] when frame_number = 10 -> x + y
+  | [x; y] when n = 10 -> x + y
   | [_; _] -> 0
   | 10 :: y :: z :: rest ->
-      frame_score 10 y z + score ~frame_number:next_frame_number (y :: z :: rest)
+      frame_score 10 y z + score ~n:(succ n) (y :: z :: rest)
   | x :: y :: z :: rest ->
-      frame_score x y z + score ~frame_number:next_frame_number (z :: rest)
+      frame_score x y z + score ~n:(succ n) (z :: rest)
