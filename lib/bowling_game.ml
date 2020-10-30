0f7a24577580b1cb32a054f3bd9f0ebbf482ce3a
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 20:57:35 +0100

    r Add is_last_frame_function

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -4,13 +4,17 @@ let is_strike =
 let is_spare x y =
   x + y = 10
 
+let is_last_frame =
+  (==) 10
+
 let rec score ?(n = 1) = function
   | [] -> 0
   | [_] -> failwith "wrong number of rolls"
-  | [x; y; z] when n = 10 -> x + y + z
+  | [x; y; z] when is_last_frame n ->
+      x + y + z
   | x :: y :: z :: rest when is_strike x ->
-    10 + y + z + score ~n:(succ n) (y :: z :: rest)
+      10 + y + z + score ~n:(succ n) (y :: z :: rest)
   | x :: y :: z :: rest when is_spare x y  ->
-    10 + z + score ~n:(succ n) (z :: rest)
+      10 + z + score ~n:(succ n) (z :: rest)
   | x :: y :: rest ->
-     x + y + score ~n:(succ n) rest
+      x + y + score ~n:(succ n) rest
