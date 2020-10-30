dbb24e09f6eba54dd9dfc2e010803a2f02ad00f3
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 20:41:03 +0100

    r Simplify strike or spare in last frame clause

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,9 +1,8 @@
 let rec score ?(n = 1) rolls =
   match rolls with
   | [] -> 0
-  | [_] -> 0
-  | [x; y] when n = 10 -> x + y
-  | [_; _] -> 0
+  | [_] -> failwith "wrong number of rolls"
+  | [x; y; z] when n = 10 -> x + y + z
   | 10 :: y :: z :: rest ->
     10 + y + z + score ~n:(succ n) (y :: z :: rest)
   | x :: y :: z :: rest when x + y = 10 ->
