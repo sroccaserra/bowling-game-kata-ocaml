e603c6f26f8b0af5c88183a50fb4720782c8bb5d
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-31 02:26:00 +0100

    r Make private functions nested

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -1,20 +1,16 @@
-let is_strike =
-  (==) 10
-
-let is_spare x y =
-  x + y = 10
-
-let is_last_frame =
-  (==) 10
-
-let rec score ?(n = 1) = function
+let rec score ?(n: int = 1) (xs: int list): int =
+  let n' = succ n in
+  let is_strike = (==) 10 in
+  let is_spare x y = x + y == 10 in
+  let is_last_frame = (==) 10 in
+  match xs with
   | [] -> 0
   | [x; y; z] when is_last_frame n ->
       x + y + z
   | x :: y :: z :: rest when is_strike x ->
-      10 + y + z + score ~n:(succ n) (y :: z :: rest)
+      10 + y + z + score ~n:n' (y :: z :: rest)
   | x :: y :: z :: rest when is_spare x y  ->
-      10 + z + score ~n:(succ n) (z :: rest)
+      10 + z + score ~n:n' (z :: rest)
   | x :: y :: rest ->
-      x + y + score ~n:(succ n) rest
+      x + y + score ~n:n' rest
   | [_] -> failwith "wrong number of rolls"
