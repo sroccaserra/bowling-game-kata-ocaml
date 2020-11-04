e59b82bb80387f65095ef11ac06a5d23de6869d5
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-11-04 17:23:44 +0100

    r Remove some redundant (?) type declarations

--- lib/bowling_game.ml
+++ lib/bowling_game.ml
@@ -3,18 +3,16 @@ type roll = Roll of int
 type frame = Frame of int
 let next (Frame n) = Frame (n + 1)
 
-let rec score ?(f:frame=Frame 1) (xs:roll list) : int =
-  let f' = next f in
-  match xs with
+let rec score ?(f=Frame 1) = function
   | [] -> 0
   | [Roll x; Roll y; Roll z] when is_last_frame f ->
       x + y + z
   | Roll x :: Roll y :: Roll z :: rest when is_strike x ->
-      10 + y + z + score ~f:f' (Roll y :: Roll z :: rest)
+      10 + y + z + score ~f:(next f) (Roll y :: Roll z :: rest)
   | Roll x :: Roll y :: Roll z :: rest when is_spare x y  ->
-      10 + z + score ~f:f' (Roll z :: rest)
+      10 + z + score ~f:(next f) (Roll z :: rest)
   | Roll x :: Roll y :: rest ->
-      x + y + score ~f:f' rest
+      x + y + score ~f:(next f) rest
   | [_] -> failwith "wrong number of rolls"
 and is_strike = (==) 10
 and is_spare x y = x + y == 10
